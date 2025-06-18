228a91cc6ba5e3cd9b92bbfcd872ebdeb8c31020
Septian Parapak <septianparapak@gmail.com>
2025-06-18 11:52:42 +0700

    Feat: Added SimpleLogForwarder for TCP and UDP packet sending

---
+++ win64-SimpleLogForwarder/SimpleLogForwarder/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace SimpleLogForwarder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.cmbProtocol = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(562, 405);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(156, 33);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(168, 26);
+            this.txtIP.TabIndex = 1;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(382, 33);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(99, 26);
+            this.txtPort.TabIndex = 2;
+            // 
+            // cmbProtocol
+            // 
+            this.cmbProtocol.FormattingEnabled = true;
+            this.cmbProtocol.Location = new System.Drawing.Point(568, 31);
+            this.cmbProtocol.Name = "cmbProtocol";
+            this.cmbProtocol.Size = new System.Drawing.Size(121, 28);
+            this.cmbProtocol.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Destination IP :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(334, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Port :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(487, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Protocol :";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(37, 133);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(652, 231);
+            this.txtMessage.TabIndex = 7;
+            this.txtMessage.Text = "CEF:0|AcmeCorp|AcmeFirewall|1.0|100|User login successful|3|src=10.100.34.131 dst" +
+    "=10.10.10.10 suser=jdoe duser=admin msg=User login succeeded";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Message :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label5.Location = new System.Drawing.Point(37, 447);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(208, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Brought to you by XYPERIA";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 495);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbProtocol);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Simple Log Forwarder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.ComboBox cmbProtocol;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox txtMessage;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ win64-SimpleLogForwarder/SimpleLogForwarder/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleLogForwarder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            cmbProtocol.Items.Add("UDP");
+            cmbProtocol.Items.Add("TCP");
+            cmbProtocol.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ip = txtIP.Text.Trim();
+            string portText = txtPort.Text.Trim();
+            string protocol = cmbProtocol.SelectedItem.ToString();
+            string message = txtMessage.Text.Trim();
+
+            if (!IPAddress.TryParse(ip, out IPAddress ipAddress))
+            {
+                MessageBox.Show("Invalid IP address.");
+                return;
+            }
+
+            if (!int.TryParse(portText, out int port) || port < 1 || port > 65535)
+            {
+                MessageBox.Show("Invalid port number.");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(message))
+            {
+                MessageBox.Show("Message cannot be empty.");
+                return;
+            }
+
+            try
+            {
+                byte[] data = Encoding.UTF8.GetBytes(message);
+
+                if (protocol == "UDP")
+                {
+                    using (UdpClient udpClient = new UdpClient())
+                    {
+                        udpClient.Send(data, data.Length, ip, port);
+                    }
+                }
+                else if (protocol == "TCP")
+                {
+                    using (TcpClient tcpClient = new TcpClient())
+                    {
+                        tcpClient.Connect(ip, port);
+                        using (NetworkStream stream = tcpClient.GetStream())
+                        {
+                            stream.Write(data, 0, data.Length);
+                        }
+                    }
+                }
+
+                MessageBox.Show("Message sent successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to send message: {ex.Message}");
+            }
+        }
+    }
+
+}
